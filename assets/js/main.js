654bcbfee1aa4da7500e92dd3de07a74e40c787d
damirpopo <43522476+damirpopo@users.noreply.github.com>
2022-10-24 13:40:31 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,147 @@
+//alert("Я js");
+//console.log('я js');
+
+//let name = prompt("Ваше имя", "Саня");
+//alert(`ваше имя ${name}`);
+
+// let a = 1, b = 1;
+ //let c = ++a;
+ //let d = b++;
+ //console.log(a)
+ //console.log(b)
+ //console.log(c)
+ //console.log(d)
+
+ //let a = 2;
+ //let x = 1 + (a *= 2);
+ //console.log(a)
+ //console.log(x)
+ 
+// let i1 = "" + 1 + 0
+ //let i2 = "" - 1 + 0
+ //let i3 = true + false
+ //let i4 =  6 / "3"
+ //let i5 =  "2" * "3"
+ //let i6 =  4 + 5 +"px"
+ //let i7 =  "$" + 4 + 5
+ //let i8 =  "4" - 2
+ //let i9 =  "4px" - 2
+ //let i10 =  " -9 " + 5
+ //let i11 =  " -9 " - 5
+ //let i12 =  null + 1
+ //let i13 =  undefined + 1
+ //let i14 =  " \t \n - 2"
+ //console.log(i1)
+ //console.log(i2)
+ //console.log(i3)
+ //console.log(i4)
+ //console.log(i5)
+ //console.log(i6)
+ //console.log(i7)
+ //console.log(i8)
+ //console.log(i9)
+ //console.log(i10)
+ //console.log(i11)
+ //console.log(i12)
+ //console.log(i13)
+ //console.log(i14)
+
+
+// let a = +prompt("Первое число?", 1);
+ //let b = +prompt("Второе число?", 1);
+ //c = a+b
+ //alert(c); 
+ 
+
+ //let num = 3
+// alert( num)
+
+//let a = 10
+//let b =2
+//c= a+b
+//d= a-b
+//e= a*b
+//f= a/b
+//console.log(c)
+//console.log(d)
+//console.log(e)
+//console.log(f)
+
+
+//let c = 15
+//let d = 2 
+//result = c+d
+//alert(result)
+
+
+
+//let a = 10;
+//let b = 2;
+//let c =5;
+//x= a+b+c
+//alert(x)
+
+//let a=17;
+//let b=10;
+//c = a-b;
+//let d=7;
+//result=c+d;
+//alert(result)
+
+//let str = 'Privet, Mir!'
+//alert(str)
+
+//let str1 = 'Privet,'
+//let str2 = 'Mir!'
+//alert(str1+str2)
+
+//let name=prompt ( 'Ваше имя', 'Саша');
+//alert(`Privet, ${name}`)
+
+//let age = prompt('Ваш возраст?', 100);
+//alert(`Mne ${age} let!`)
+
+//let name=prompt ( 'Ваше имя', 'Саша');
+//alert(`Vashe ime ${name}`)
+
+
+//let i = prompt('Число', 2);
+//alert(`его квадрат ${i**2}`)
+
+//str=("abcde");
+//alert (str[0]);
+//alert (str[2]);
+//alert (str[4]);
+
+
+let num=('12345');
+alert(num[0]*num[1]*num[2]*num[3]*num[4]);
+
+
+//let hour = 60*1
+//let sutki = 24*hour
+//let mesec = 30*sutki
+//console.log(hour)
+//console.log(sutki)
+//console.log(mesec)
+
+
+
+
+
+//let i = prompt('Число', 2);
+//alert(`его квадрат ${i**2}`)
+
+//var num =47;
+//num +=7;
+//num -=18
+//num *=10
+//num /=15
+//alert(num)
+
+//var num =10;
+//num ++
+//num ++
+//num --
+//alert(num);
+
